07ed6b749d8d2a16818b6c139f6c8ff58529441f
Kacper Kwietniewski <kkwietniewski@edu.cdv.pl>
2020-11-03 13:27:01 +0100

    Poprawione zad_1_rectangle_square

--- zad_1_rectangle_square.py
+++ zad_1_rectangle_square.py
@@ -1,14 +1,16 @@
-class Square:
-    def calcSquareArea(self,x):
-        return x*x
-
-square = Square()
-print(square.calcSquareArea(4))
-
-class Rectangle(Square):
-    def calcRectangleArea(self,x,y):
-        return x*y
-    
-rectangle = Rectangle()
-print("Pole kwadratu z dziecka klasy Rectangle: ",rectangle.calcSquareArea(6))
-print("Pole prostokąta rodzica: ",rectangle.calcRectangleArea(6,5))
+class Rectangle:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+    def calcArea(self):
+        return self.x*self.y
+			
+class Square(Rectangle):
+    def __init__(self,x):
+        self.x = x
+        self.y = x
+        
+rectangle= Rectangle(6,5)
+square= Square(6)
+print("Pole kwadratu z dziecka klasy Rectangle: ",square.calcArea())
+print("Pole prostokąta rodzica: ",rectangle.calcArea())
